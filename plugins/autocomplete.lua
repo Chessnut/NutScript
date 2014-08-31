7d28ed7ce0e4e046cca94042ea38f7c0f3d8a85f
Brian <chessnutist@gmail.com>
2014-08-31 15:30:35 -0700

    Fixed auto-complete plugin (completely)

--- plugins/autocomplete.lua
+++ plugins/autocomplete.lua
@@ -10,18 +10,24 @@ if (CLIENT) then
 	
 	function PLUGIN:HUDPaint()
 		local frame = nut.chat.panel.frame
-		
+
 		if (IsValid(frame) and LocalPlayer():IsTyping()) then
 			if (chatText:sub(1, 1) == "/") then
 				local spacer = 0
 				local counter = 0
 				local x, y = frame:GetPos()
-				local height = frame:GetTall()
-				
-				for k,v in pairs(nut.command.buffer) do
-					if (chatText:sub(2, #k + 1):lower() == k and counter < 4) then
-						draw.SimpleTextOutlined("/"..k, "nut_ChatFont", x + 9, (y + tall - 6) + spacer, textColor, 0, 0, 1, outline)
-						
+				local tall = frame:GetTall()
+				local mainColor = nut.config.mainColor
+				local color = Color(mainColor.r, mainColor.g, mainColor.b)
+
+				for k, v in SortedPairs(nut.command.buffer) do
+					local k2 = "/"..k
+
+					if (k2:find(chatText:sub(1, #k2):lower()) and counter < 4) then
+						local x2, y2 = x + 9, (y + tall - 6) + spacer
+						local w = draw.SimpleTextOutlined(k2.." ", "nut_ChatFont", x2, y2, color, 0, 0, 1, outline)
+						draw.SimpleTextOutlined(v.syntax or "[none]", "nut_ChatFont", x2 + w, y2, textColor, 0, 0, 1, outline)
+
 						spacer = spacer + 17
 						counter = counter + 1
 					end
