3ebfe861d7e3eaf16e2c8223c81e346369507909
Brian <chessnutist@gmail.com>
2015-02-01 12:02:04 -0800

    Update shared.lua

--- gamemode/shared.lua
+++ gamemode/shared.lua
@@ -61,3 +61,24 @@ nut.util.IncludeDir("derma", true)
 nut.util.Include("sh_commands.lua")
 
 NSFolderName = GM.FolderName
+
+if (SERVER and game.IsDedicated()) then
+	concommand.Remove("gm_save")
+	
+	concommand.Add("gm_save", function(client, command, arguments)
+		client:ChatPrint("You are not allowed to do that, administrators have been notified.")
+
+		if ((client.nutNextWarn or 0) < CurTime()) then
+			local message = client:Name().." ["..client:SteamID().."] has possibly attempted to crash the server with 'gm_save'"
+
+			for k, v in ipairs(player.GetAll()) do
+				if (v:IsAdmin()) then
+					v:ChatPrint(message)
+				end
+			end
+
+			MsgC(Color(255, 255, 0), message.."\n")
+			client.nutNextWarn = CurTime() + 60
+		end
+	end)
+end
